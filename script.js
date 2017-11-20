80c61708941f8990dfd076e2e87254a1173ae6d8
Dariusz Nurzyński <scorpion7812@gmail.com>
2017-11-20 16:13:26 +0100

    Basic Version

--- script.js
+++ script.js
@@ -1,169 +1,90 @@
-var person = {
-    name: "Janusz",
-    surname: "Biznes",
-    city: "New York",
-    age: 18,
-    display: function() {
-        return this.name + ", " + this.surname + ", " + this.city + ", " + this.age;
-    }
-};
+var currencies = [
+    { 'name': 'Polski złoty', 'code': "PLN"},
+    { 'name': 'EURO', 'code': "EUR"},
+    { 'name': 'Frank Szwajcarski', 'code': "CHF"},
+    { 'name': 'Korona Czeska', 'code': "CZK"},
+    { 'name': 'Rubel Rosyjski', 'code': "RUB"},
+    { 'name': 'Hryvna Ukraińska', 'code': "UAH"},
+    { 'name': 'Funt Brytyjski', 'code': "GBP"},
+    { 'name': 'Korony Norweskie', 'code': "NOK"},
+    { 'name': 'Filipińskie Pesso', 'code': "PHP"},
+    { 'name': 'Dolar Amerykański', 'code': "USD"}
+];
 
-var dyskD = {
-    pliki : ["plik1", "plik2", "plik3"],
-    katalogi : [
-        katalog1 = {
-            pliki : ["plik3", "plik4", "plik5"],
-            katalogi : {
-                katalog3 : {
-                    pliki : ["plikx", "pliky"]
-                }
-            }
-        },
-        katalog2 = {
-            pliki : ["plik8", "plik9"]
-        }
-    ]
-};
+var sourceCurrencyList = document.getElementById("source-currency");
+var targetCurrencyList = document.getElementById("target-currency");
+var moneyValue = document.getElementById("money-value");
+var result = document.getElementById("result");
+var button = document.getElementById("button");
+var form = document.getElementById("form");
 
+form.onsubmit = function(event) {
+    event.preventDefault();
+}
 
-var TargeType = {
-    FOLDER : 0,
-    FILE : 1
-};
-
-var personTestButton = document.getElementById("object_test_button");
-var personResult = document.getElementById("object_result");
-
-var expRecButton = document.getElementById("recuration_rec_button");
-var expItButton = document.getElementById("recuration_it_button"); 
-
-var baseVal = document.getElementById("base_value");
-var exponentVal = document.getElementById("exponent_value");
-var expResult = document.getElementById("expResult");
-
-var polimeterPolygonButton = document.getElementById("perimeterPolygonButton"); 
-var firstSide = document.getElementById("first_side");
-var secondSide = document.getElementById("second_side");
-var thirdSide = document.getElementById("third_side");
-var resultPerimeter = document.getElementById("perimeterResult");
+for (var x in currencies) {
+    var opt = document.createElement("option");
+    opt.value = x;
+    opt.textContent = currencies[x].name + " (" + currencies[x].code + ")";
+    sourceCurrencyList.appendChild(opt);
+}
 
 
-var divisibilityButton = document.getElementById("divisibilityButton"); 
-var a = document.getElementById("a_val");
-var b = document.getElementById("b_val");
-var divisibilityResult = document.getElementById("divisibilityResult");
 
+for (x = currencies.length - 1; x >= 0; x--) {
+    var opt = document.createElement("option");
+    opt.value = x;
+    opt.textContent = currencies[x].name + " (" + currencies[x].code + ")";
+    targetCurrencyList.appendChild(opt);
+}
 
-var searchButton = document.getElementById("searchButton"); 
-var serchedName = document.getElementById("searched_name");
-var driveView = document.getElementById("object_drive_view");
-var findingResult = document.getElementById("findingResult");
+button.onclick = function() {
 
+    let indexSource = sourceCurrencyList.value;
+    let indexTarget = targetCurrencyList.value;
+    let check = validate(indexSource, indexTarget);
 
-personTestButton.addEventListener("click", function () {
-    personResult.innerHTML = person.display();
-});
-
-expRecButton.addEventListener("click", function() {
-    let result = expRec(baseVal.value, exponentVal.value);
-    expResult.innerHTML = "Rekurencyjnie: "  + result;
-});
-
-expItButton.addEventListener("click", function() {
-    let result = expIt(baseVal.value, exponentVal.value);
-    expResult.innerHTML = "Iteracyjnie: "  + result;
-});
-
-polimeterPolygonButton.addEventListener("click", function() {
-    let result = perimeterPolygon(firstSide.value, secondSide.value, thirdSide.value);
-    resultPerimeter.innerHTML = result;
-}); 
-
-divisibilityButton.addEventListener("click", function() {
-    let result = divisibility(a.value, b.value);
-    divisibilityResult.innerHTML = result;
-}); 
-
-driveView.innerHTML = JSON.stringify(dyskD, undefined, 5);
-searchButton.addEventListener("click", function() {
-    find(dyskD, serchedName.value);
-    
-    let result = "";
-    if (isFound) {
-        result = "Podany plik/katalog znajduje się w przestrzeni.";
+    if (check != 'OK') {
+        result.innerHTML = check;
     }
     else {
-        result = "Brak elementów";
+        let rate = sendReqForRate(indexSource, indexTarget);
+        let kwota = moneyValue.value;
+        let output = kwota + " " + currencies[indexSource].code + " wynosi " + (kwota * rate).toFixed(2) + " " + currencies[indexTarget].code + ".";
+        result.innerHTML = output;
     }
-    
-    findingResult.innerHTML = result;
-});
-
-function expRec(base, exponent) {
-    if (exponent == 0) {
-        return 1;
-    }
-    else {
-        return base * expRec(base, --exponent);
-    }
-}
-
-function expIt(base, exponent) {
-    if (exponent == 0) {
-        return 1;
-    }
-    let result = 1;
-    
-    for (let i = 1; i <= exponent; i++) {
-        result *= base;
-    }
-    
-    return result;
-}
-
-function perimeterPolygon() {
-    let result = 0;
-    
-    for (let x of arguments) {
-        result += (+x);
-    }
-    
-    return result;
-}
-
-function divisibility(a, b) {
-    
-    function check() {
-        if (a % b == 0) return 'a jest podzielne przez b';
-        else return 'a nie jest podzielnie przez b';
-        
-    }
-    
-    return check();
     
 }
 
-var isFound = false;
-
-function find(root,name) {
+function validate(indexSource, indexTarget) {
     
-    for (var element in root) {
-        
-        alert(element);
-        
-        if ((typeof root[element] === 'object') || Array.isArray(root[element])) {
-            if (element == name) {
-                isFound = true;
-                return;
-            }
-            
-            alert("Szukam głębiej");
-            find(root[element], name);
-        }
-        
-        else if (root[element] == name) {  
-            alert("Odnaleziono");
-            isFound = true;
-        }
+    if (indexSource == indexTarget) {
+        return "Waluty powinny się różnić";
     }
+    
+    var kwota = moneyValue.value;
+    kwota = kwota.replace(/\s+/g, '');
+    alert("1" + kwota + "1");
+    if (isNaN(kwota) || kwota == "") {
+        return "Wartość kwoty jest błędna";
+    }
+
+    return 'OK';
 }
+
+function sendReqForRate(indexS, indexT) {
+    let base = currencies[indexS].code;
+    let target = currencies[indexT].code;
+
+    let url = "https://api.fixer.io/latest?base=" + base +"&symbols=" + target;
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false );
+    xmlHttp.send( null );
+    let response =JSON.parse(xmlHttp.responseText);
+
+    return response.rates[target];
+} 
+
+
+
